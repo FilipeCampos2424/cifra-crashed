c613f1c8cb5bcd1986da4a2033933023ed4116ad
FilipeCampos242424 <filipegames2343@gmail.com>
2026-03-31 08:44:35 -0300

    Criação do arquivo Main, iniciando o código, e começando a função de tratar a frase (tirar ascento, espaço e deixar tudo maiúsculo

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+    Scanner leia = new Scanner(system.in);
+
+    system.out.println("Escreva uma fase: ");
+    String fraseOriginal = leia.nextLine();
+    //Tratamento da frase
+    String fraseConvertida = fraseOriginal.toUpperCase();
+
+    }
+}
