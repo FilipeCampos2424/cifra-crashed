7c3bd27ff244b942c254f67708fe917305dc68ff
FilipeCampos2424 <filipegames2343@gmail.com>
2026-04-07 19:20:39 -0300

    Menu pra escolher cifrador e decifrador adicionado, cifrador completo + comentários do código

--- Main.java
+++ Main.java
@@ -1,15 +1,47 @@
 import java.util.Scanner;
+import java.text.Normalizer; //só pra tratar os acentos
 
 public class Main{
     public static void main(String[]args){
     Scanner leia = new Scanner(System.in);
 
-    System.out.println("Escreva uma fase: ");
+    System.out.println("CIFRADOR E DECIFRADOR JAVA\n");
+    System.out.println("(1) Cifrador (2) Decifrador\n");
+    System.out.println("Escolha: ");
+    int escolher = leia.nextInt();
+
+    leia.nextLine();
+
+    if (escolher == 1) {
+
+    System.out.println("Escreva uma frase: ");
     String fraseOriginal = leia.nextLine();
     //Tratamento da frase
-    String fraseConvertida = fraseOriginal.toUpperCase().replaceAll("\\s", "");
+    //Normalizer usado pra tratar a frase certinho
+    String fraseConvertida = Normalizer.normalize(fraseOriginal, Normalizer.Form.NFD); // o NFD separa o acento da letra
+    fraseConvertida = fraseConvertida.replaceAll("\\p{M}", "") //tira os acentos
+    .replaceAll("[^a-zA-Z0-9]", "").toUpperCase(); //toUpperCase pra deixar tudo maiusculo
+    //ele nao vai escrever nada que nao for de a-z, A-Z ou 0-9
+
     System.out.println(fraseConvertida);
 
+    //n pega o tamanho da frase convertida
+    int n = fraseConvertida.length();
+    //o indice que vai fazer os saltos
+    int indice = 0;
+    //vetor resultado que guarda o tamanho da frase em caracteres
+    char resultado[] = new char[n];
+    
+    //for para percorrer o vetor
+    for (int i = 0; i < n; i++) {
+        //o vetor em for vira igual a frase em char de acordo com o indice
+        resultado[i] = fraseConvertida.charAt(indice);
+        //o indice que faz os saltos até o tamanho da string
+        indice = (indice + 5) % n;
+    }
+
+    System.out.println(resultado);
+      }
 
     }
 }
